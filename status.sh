2f2effb0d46e7a59f65559d7bc21d726cd311f62
chocomilku <88021521+chocomilku@users.noreply.github.com>
2025-03-30 20:06:36 +0800

    added memory_check, cpu_check, disk_check, kernel_check to status.sh

--- status.sh
+++ status.sh
@@ -1,4 +1,35 @@
 #!/usr/bin/env bash
 
 server_name=$(hostname)
-echo "Server Name: ${server_name}" 
+echo "Server Name: ${server_name}"
+
+function memory_check() {
+    echo ""
+    echo "Current Memory Usage: " 
+    free -h
+    echo ""
+}
+
+function cpu_check() {
+    loadavg="$(cat /proc/loadavg)"
+    echo "Current CPU load: "
+    echo "${loadavg}"
+    echo ""
+}
+
+function disk_check() {
+    echo "Current Disk Usage: "
+    df -Thi
+    echo ""
+}
+
+function kernel_check() {
+   echo "Current Kernel Version: "
+   uname -r
+   echo ""
+}
+
+memory_check
+cpu_check
+disk_check
+kernel_check
